b82069b3f05d989dc099f763745c76b127705806
gusdev <59888502+Gus2708@users.noreply.github.com>
2026-01-15 04:48:36 +0000

    Traffic Light project finished✅ - Traffic light simulation with html and css ✅ - lights change functionality ✅ - Bonus ➕: Purple light added with button to activate it

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [turn, setTurn] = useState("red");
+	const [purple, setPurple] = useState(false);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (purple) {
+				setTurn(prev =>
+					prev === "red" ? "green" : prev === "green" ? "purple" : prev === "purple" ? "yellow" : "red"
+				);
+			} else {
+				setTurn(prev =>
+					prev === "red" ? "green" : prev === "green" ? "yellow" : "red"
+				);
+			};
+
+		}, 3000,);
+
+		return () => clearInterval(interval);
+	}, [purple]);
+
+	useEffect(() => {
+		document.documentElement.style.setProperty("--selected-color", turn);
+	}, [turn]);
+
+	return (
+		<div className="container-fluid d-flex align-items-center flex-column">
+			<div className="trafficLightBase"></div>
+			<div className="trafficLight d-flex justify-content-evenly flex-column align-items-center">
+				<div
+					className={`red light ${turn === "red" ? "selected" : ""}`}
+					onClick={() => setTurn("red")}
+				></div>
+				<div
+					className={`yellow light ${turn === "yellow" ? "selected" : ""}`}
+					onClick={() => setTurn("yellow")}
+				></div>
+				<div
+					className={`green light ${turn === "green" ? "selected" : ""}`}
+					onClick={() => setTurn("green")}
+				></div>
+				{purple == true ?
+					<div
+						className={`purple light ${turn === "purple" ? "selected" : ""}`}
+						onClick={() => setTurn("purple")}
+					></div> : ""}
+			</div>
+			<div className="container-fluid mt-2 d-flex justify-content-center">
+				<button 
+					type="button"
+					className="btn btn-outline-primary purple"
+					onClick={() => {
+						setPurple(!purple)
+						purple === true ? setTurn("red") : "";
+					}}
+				>Purple Button?</button>
+			</div>
 		</div>
 	);
 };
